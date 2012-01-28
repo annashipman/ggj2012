b27c6e2fb297a77dbec233fa4707bb609e923f5f
Anna Shipman <annashipman@hotmail.co.uk>
2012-01-28 19:00:46 +0000

    kind of scrolling background...

--- SGJ2012/Assets/MainLoop.js
+++ SGJ2012/Assets/MainLoop.js
@@ -4,7 +4,8 @@ public var snake: GameObject;
 public var blocks : GameObject[] = new GameObject[10];
 private var levels: Transform[] = new Transform[100];
 private var privateBlocks : GameObject[] = new GameObject[10];
-private var mostRightBlock : int;
+private var rightBlock : int;
+private var leftBlock : int;
 private var newBlockTrigger : int;
 
 function Start(){
@@ -13,7 +14,7 @@ cam = Camera.main;
 for (var i : int = 0;i < 3; i++) {
 var groundBlock = Instantiate (this.blocks[0], Vector3((i * 10.0), -6.0, 0), Quaternion.identity);
 privateBlocks[i] = groundBlock;
-this.mostRightBlock = i;
+this.rightBlock = i;
 }
 
 this.newBlockTrigger = 18;
@@ -30,11 +31,13 @@ cam.transform.position = Vector3.Lerp(cam.transform.position, nepos, (Time.time 
 ///camera posistion!!!!!!!
 //figure out if we are reaching the edge of the screen
 if (snake.transform.position.x > this.newBlockTrigger ){
-		var i = this.mostRightBlock;
-		this.mostRightBlock = this.mostRightBlock+1; //unless it's less than 10
-		var groundBlock = Instantiate (this.blocks[0], Vector3((this.mostRightBlock * 10.0), -6.0, 0), Quaternion.identity);
+		var i = this.rightBlock;
+		this.rightBlock = this.rightBlock+1; //unless it's less than 10
+		var groundBlock = Instantiate (this.blocks[leftBlock], Vector3((this.rightBlock * 10.0), -6.0, 0), Quaternion.identity);
 		privateBlocks[i] = groundBlock;
 		this.newBlockTrigger = this.newBlockTrigger + 10;
+		this.leftBlock = this.rightBlock - 3;
+		Destroy(privateBlocks[leftBlock]);
 }
 }
 
