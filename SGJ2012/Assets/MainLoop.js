7ba4641a5544e065d4635a1f6500ee3741939af8
Anna Shipman <annashipman@hotmail.co.uk>
2012-01-28 22:56:15 +0000

    possibly the worst code I've written since this time last year...

--- SGJ2012/Assets/MainLoop.js
+++ SGJ2012/Assets/MainLoop.js
@@ -2,20 +2,21 @@
 public var levenum = 0;
 public var cam;
 public var snake: GameObject;
-public var blocks : GameObject[] = new GameObject[10];
+public var blocks : GameObject[] = new GameObject[20];
 private var levels: Transform[] = new Transform[100];
-private var privateBlocks : GameObject[] = new GameObject[10];
-private var rightBlock : int;
-private var leftBlock : int;
+private var privateBlocks : GameObject[] = new GameObject[200];
+private var endBlock : int;
 private var newBlockTrigger : int;
+private var blockCounter : int;
 
 function Start(){
 cam = Camera.main;
-
-for (var i : int = 0;i < 3; i++) {
-var groundBlock = Instantiate (this.blocks[0], Vector3((i * 10.0), -6.0, 0), Quaternion.identity);
+Debug.Log(blocks[0]);
+for (var i : int = 1;i < 3; i++) {
+var groundBlock = Instantiate (this.blocks[i], Vector3((i * 10.0), -6.0, 0), Quaternion.identity);
 privateBlocks[i] = groundBlock;
-this.rightBlock = i;
+this.endBlock = i;
+this.blockCounter = i;
 }
 
 this.newBlockTrigger = 18;
@@ -33,21 +34,36 @@ cam.transform.position = Vector3.Lerp(cam.transform.position, nepos, (Time.time 
 //figure out if we are reaching the edge of the screen
 //if the snake x has got past halfway through the 3rd block
 if (snake.transform.position.x > this.newBlockTrigger ){
-		//give var i the index of the third block, i.e. 2
-		var i = this.rightBlock;
-		//increment the right block, so 3
-		this.rightBlock = this.rightBlock+1; //unless it's less than 10
-		//create a game object called ground block
-		//Clones the object original, places it at position and sets the rotation to rotation,
-		//Original object is   position is   rotation is Quaternion.identity, i.e. This quaternion corresponds to "no rotation": the object 
-		//then returns the cloned object. This is essentially the same as using duplicate command 
-		//(cmd-d) in Unity and then moving the object to the given location
-		
-		var groundBlock = Instantiate (this.blocks[leftBlock], Vector3((this.rightBlock * 10.0), -6.0, 0), Quaternion.identity);
-		privateBlocks[i] = groundBlock;
+		//reset new block trigger
 		this.newBlockTrigger = this.newBlockTrigger + 10;
-		this.leftBlock = this.rightBlock - 3;
-		Destroy(privateBlocks[leftBlock]);
+		
+		//get the x of the current block
+		Debug.Log(this.endBlock);
+		//update the block to instantiate
+		if (this.endBlock < 9) {
+		Debug.Log("updating");
+		this.endBlock = this.endBlock + 1;
+		} else {
+		Debug.Log("9");
+		this.endBlock = 0;
+		}
+		//update block counter
+		this.blockCounter = this.blockCounter + 1;
+		Debug.Log(this.blockCounter);
+		//Instantiate the end block
+		
+		//Clones the object original, places it at position and sets the rotation to rotation,
+		//Original object is the first block in the array (?) position is 10 to the right of the 
+		//previous one, rotation is Quaternion.idenitity, i.e. no rotation - the object 
+		//then returns the cloned object. 
+		var groundBlock = Instantiate (this.blocks[this.endBlock], Vector3((this.blockCounter * 10.0), -6.0, 0), Quaternion.identity);
+		//then add the new block to the private blocks array	
+		privateBlocks[this.endBlock] = groundBlock;
+	
+		//then destroy the first block - really? we might need it again!
+		//uninstantiate?
+		//or can destroy as it's a prefab?
+		//Destroy(privateBlocks[leftBlock]);
 }
 }
 
