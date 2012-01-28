ea30b11fa9b095e515d668755bee7ab5b0b04843
Anna Shipman <annashipman@hotmail.co.uk>
2012-01-28 18:13:29 +0000

    add one extra tile...

--- SGJ2012/Assets/MainLoop.js
+++ SGJ2012/Assets/MainLoop.js
@@ -5,6 +5,7 @@ public var blocks : GameObject[] = new GameObject[10];
 private var levels: Transform[] = new Transform[100];
 private var privateBlocks : GameObject[] = new GameObject[10];
 private var mostRightBlock : int;
+private var newBlockTrigger : int;
 
 function Start(){
 cam = Camera.main;
@@ -12,8 +13,10 @@ cam = Camera.main;
 for (var i : int = 0;i < 3; i++) {
 var groundBlock = Instantiate (this.blocks[0], Vector3((i * 10.0), -6.0, 0), Quaternion.identity);
 privateBlocks[i] = groundBlock;
-//mostRightBlock = i;
+this.mostRightBlock = i;
 }
+
+this.newBlockTrigger = 18;
 
 	
 }
@@ -25,10 +28,15 @@ nepos.z -= 10.0;
 cam.transform.position = Vector3.Lerp(cam.transform.position, nepos, (Time.time - 0.6) / 1.0);
 
 ///camera posistion!!!!!!!
-
+Debug.Log(this.mostRightBlock + " trig " + this.newBlockTrigger + " x " + snake.transform.position.x);
 //figure out if we are reaching the edge of the screen
-if (snake.transform.position.x > 18 ){
-//Debug.Log("Hello" + this.mostRightBlock);
+if (snake.transform.position.x > this.newBlockTrigger ){
+		var i = this.mostRightBlock;
+		this.mostRightBlock = this.mostRightBlock+1; //unless it's less than 10
+		var groundBlock = Instantiate (this.blocks[i], Vector3((this.mostRightBlock * 10.0), -6.0, 0), Quaternion.identity);
+		privateBlocks[i] = groundBlock;
+		this.newBlockTrigger = this.newBlockTrigger + 10;
+		Debug.Log(this.mostRightBlock + " trig " + this.newBlockTrigger);
 }
 }
 
